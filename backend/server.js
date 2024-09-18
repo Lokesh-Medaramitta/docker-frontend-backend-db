0e193a50bb41b6fb4d10ee8e8738471cdbe5e395
Lokesh-Medaramitta <144916985+Lokesh-Medaramitta@users.noreply.github.com>
2024-09-18 16:08:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ async function main() {
   const app = express();
   app.use(cors());
   app.use(express.json());
-  app.use("/api", routes);
+  app.use(routes);
 
   app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
