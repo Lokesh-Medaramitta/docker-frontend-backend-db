f7a71f8e842aed8b5f93d8294b15a05711f2d657
Lokesh-Medaramitta <144916985+Lokesh-Medaramitta@users.noreply.github.com>
2024-10-08 12:50:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const routes = require("./routes");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://database:27017/todos", {
+  await mongoose.connect("mongodb://username:password@database:27017/todos", {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   });
